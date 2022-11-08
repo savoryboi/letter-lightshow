4f739258026683a18ea44ff5065cff15a6918385
savoryboi <andbjerk@yahoo.com>
2022-11-08 14:02:37 -0600

    added flash warning and mild transition to color changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 var letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var pallete = ['#B0F81F', '#7CBA00', '#E5FC23', '#FFD300', '#00A100', '#00DE00','#3AD08C', '#00EC81', '#00E7E7','#0089EA', '#0752FF', '#EE01FF','#FF00BF', '#AA03FF', '#F9B446','#FF9C00', '#EC425D', '#FF0028','#F9E946', '#DAFA99', '#D2F983','#BB95E7', '#E08DE6', '#FF1800'];
 
+const warning = confirm('WARNING: This application contains flashing/changing lights and colors that may cause nausea or ceizures for some.')
 
 $(document).on('keydown', function(event){
     var randColor = pallete[Math.floor(Math.random() * pallete.length)] ;
